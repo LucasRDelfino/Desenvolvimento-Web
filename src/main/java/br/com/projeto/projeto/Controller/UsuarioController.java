9041fd347e33bbfdd918a5aaebb426072dc12a00
LucasRDelfino <lucasrdelfino2005@gmail.com>
2021-11-16 20:24:11 -0300

    Atualizando o projeto

--- src/main/java/br/com/projeto/projeto/Controller/UsuarioController.java
+++ src/main/java/br/com/projeto/projeto/Controller/UsuarioController.java
@@ -3,8 +3,13 @@ package br.com.projeto.projeto.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,12 +18,16 @@ import java.util.Optional;
 import br.com.projeto.projeto.beans.Usuario;
 import br.com.projeto.projeto.DAO.UsuarioDAO;
 
-@RestController
+@Controller
 public class UsuarioController {
 
 	@Autowired
 	private UsuarioDAO dao;
 	
+	@GetMapping("/ble")
+	   public String homeApp() {
+		   return "index";
+	   }
 	@GetMapping("/Usuario")
 	public ResponseEntity<List<Usuario>> getAll(){
 		List<Usuario> list = (List<Usuario>)dao.findAll();
@@ -29,6 +38,7 @@ public class UsuarioController {
 		
 		return ResponseEntity.ok(list);
 	}
+	
 	@GetMapping("/lucas{userId}")
     public ResponseEntity<Optional<Usuario>> getAllById(@PathVariable("userId") int id){
         Optional<Usuario> listId = dao.findById(id);
@@ -37,5 +47,29 @@ public class UsuarioController {
 
         return ResponseEntity.ok(listId);
     }
-	
+
+	@PostMapping("/novousario")
+    public ResponseEntity<Usuario> addUsuario(@RequestBody Usuario objeto){
+       try {
+        	dao.save(objeto);
+        	return ResponseEntity.status(403).build();
+        }catch(Exception e) {
+        	e.printStackTrace();
+        	return ResponseEntity.status(403).build();
+        }
+
+	}   
+    
+
+   @PostMapping("/login")
+   public ResponseEntity<Usuario> logar(@RequestBody Usuario objeto){
+	   Usuario resposta = dao.findByEmailAndSenha(objeto.getEmail(),objeto.getSenha());
+	   
+	   if (resposta==null) {
+		   return ResponseEntity.status(404).build();
+	   }
+   
+   return ResponseEntity.ok(objeto);
+}	
+  
 }
--- src/main/java/br/com/projeto/projeto/DAO/UsuarioDAO.java
+++ src/main/java/br/com/projeto/projeto/DAO/UsuarioDAO.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import br.com.projeto.projeto.beans.Usuario;
 
-public interface UsuarioDAO extends CrudRepository<Usuario, Integer>{ 
 
+public interface UsuarioDAO extends CrudRepository<Usuario, Integer>{ 
+	
+	public Usuario findByEmailAndSenha(String email,String senha);
 }
